3e02075a1e2fa5db6ef2f79c15dfdac3ff55d885
malucard <malucart@outlook.com>
2025-03-13 05:07:02 -0300

    improve concat2k detection

--- kidfile/src/archive_formats/concat2k.rs
+++ kidfile/src/archive_formats/concat2k.rs
@@ -1,12 +1,14 @@
-use crate::{file_data::FileData, Certainty, Decoder};
+use crate::{Certainty, Decoder};
 use super::{Archive, ArchiveEntry};
 
 // it's annoying that this has to exist, but here we go
 // this is a decoder that detects concatenated files based on the heuristic of...
-// finding 2048 byte boundaries following 256 null bytes and at least one non-null byte in the next 64 bytes
+// finding 2048 byte boundaries following an amount of null bytes and at least one non-null byte in the next bytes
 // really hoping that doesn't cause false positives
 
 const ALIGNMENT: usize = 2048;
+const ZEROS: usize = 64;
+const AFTER: usize = 512;
 
 pub const ENTRY_CONCAT2K: Decoder<Archive> = Decoder {
 	id: "concat2k",
@@ -14,45 +16,56 @@ pub const ENTRY_CONCAT2K: Decoder<Archive> = Decoder {
 	detect: |file| Certainty::possible_if(file.len() > ALIGNMENT * 2 && !file.starts_with(b"\x7FELF") && !file.starts_with(b"\0\0\x01\xBA") && {
 		let mut boundary = ALIGNMENT;
 		loop {
-			let mut check_buf = [0u8; 256 + 64];
-			if file.read_chunk_exact(&mut check_buf, boundary - 256).is_err() {
+			let mut check_buf = [0u8; ZEROS + AFTER];
+			if file.read_chunk_exact(&mut check_buf, boundary - ZEROS).is_err() {
 				break false;
 			}
-			if check_buf[0..256].iter().all(|x| *x == 0) && check_buf[256..256 + 64].iter().any(|x| *x != 0) {
+			if check_buf[0..ZEROS].iter().all(|x| *x == 0) && check_buf[ZEROS..ZEROS + AFTER].iter().any(|x| *x != 0) {
+				break true;
+			}
+			if check_signature(&check_buf[ZEROS..]) {
 				break true;
 			}
 			boundary += ALIGNMENT;
 		}
 	}),
-	decode
+	decode: |file| {
+		let mut cur_entry_start = 0;
+		let mut boundary = ALIGNMENT;
+		let mut entries = Vec::new();
+		loop {
+			let mut check_buf = [0u8; ZEROS + AFTER];
+			if file.read_chunk_exact(&mut check_buf, boundary - ZEROS).is_err() {
+				entries.push(ArchiveEntry {
+					name: entries.len().to_string(),
+					data: file.subfile(cur_entry_start, file.len() - cur_entry_start).unwrap(),
+					timestamp: None
+				});
+				if entries.len() > 1 {
+					return Ok(Archive {format: "concat2k", entries: entries.into()});
+				} else {
+					return Err("could not find multiple entries".into());
+				}
+			}
+			if check_signature(&check_buf[ZEROS..ZEROS + AFTER])
+					|| (check_buf[0..ZEROS].iter().all(|x| *x == 0) && check_buf[ZEROS..ZEROS + AFTER].iter().any(|x| *x != 0)) {
+				entries.push(ArchiveEntry {
+					name: entries.len().to_string(),
+					data: file.subfile(cur_entry_start, boundary - cur_entry_start).unwrap(),
+					timestamp: None
+				});
+				cur_entry_start = boundary;
+			}
+			boundary += ALIGNMENT;
+		}
+	}
 };
 
-fn decode(file: &mut FileData) -> Result<Archive, String> {
-	let mut cur_entry_start = 0;
-	let mut boundary = ALIGNMENT;
-	let mut entries = Vec::new();
-	loop {
-		let mut check_buf = [0u8; 256 + 64];
-		if file.read_chunk_exact(&mut check_buf, boundary - 256).is_err() {
-			entries.push(ArchiveEntry {
-				name: entries.len().to_string(),
-				data: file.subfile(cur_entry_start, file.len() - cur_entry_start).unwrap(),
-				timestamp: None
-			});
-			if entries.len() > 1 {
-				return Ok(Archive {format: "concat2k", entries: entries.into()});
-			} else {
-				return Err("could not find multiple entries".into());
-			}
+fn check_signature(buf: &[u8]) -> bool {
+	for sig in [b"ogdt", b"TIM2"] {
+		if buf.starts_with(sig) || buf[4..].starts_with(sig) {
+			return true;
 		}
-		if check_buf[0..256].iter().all(|x| *x == 0) && check_buf[256..256 + 64].iter().any(|x| *x != 0) {
-			entries.push(ArchiveEntry {
-				name: entries.len().to_string(),
-				data: file.subfile(cur_entry_start, boundary - cur_entry_start).unwrap(),
-				timestamp: None
-			});
-			cur_entry_start = boundary;
-		}
-		boundary += ALIGNMENT;
 	}
+	false
 }
