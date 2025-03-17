e8502967d90fce088bff1339904c84c4690770e6
malucard <malucart@outlook.com>
2025-03-17 14:42:49 -0300

    bump semver

--- kidfile/src/data_formats/mod.rs
+++ kidfile/src/data_formats/mod.rs
@@ -3,10 +3,8 @@ use super::Decoder;
 
 mod lzss;
 mod cps;
-mod lzssbe;
 
 pub const DATA_DECODERS: LazyLock<Vec<Decoder<Box<[u8]>>>> = LazyLock::new(|| [
 	lzss::ENTRY_LZSS,
-	cps::ENTRY_CPS,
-	lzssbe::ENTRY_LZSSBE
+	cps::ENTRY_CPS
 ].into());
